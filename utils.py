f3b5de772d5fc04859e9da0e474fbe053e412d01
zixiao_bios <zixiao_bios@outlook.com>
2025-07-26 17:47:39 +0800

    测试时取消动态归一化

--- utils.py
+++ utils.py
@@ -6,6 +6,11 @@ def dynamic_normalize(img):
     """根据图像中像素值的范围，动态归一化到[0, 1]"""
     return (img - img.min()) / (img.max() - img.min())
 
+def normalize(img):
+    """将[-1, 1]范围的图像归一化到[0, 1]"""
+    img = torch.clamp(img, -1, 1)
+    return (img + 1) / 2
+
 def show_step_imgs(step_imgs):
     """并排显示一系列 step 的图像，用于可视化训练过程
 
@@ -16,7 +21,7 @@ def show_step_imgs(step_imgs):
     
     fig, axs = plt.subplots(1, len(step_imgs), figsize=(15, 5), squeeze=False)
     for i, (step, img) in enumerate(step_imgs.items()):
-        x_t_pil = transforms.ToPILImage()(dynamic_normalize(img.squeeze()))
+        x_t_pil = transforms.ToPILImage()(normalize(img.squeeze()))
         axs[0, i].imshow(x_t_pil)
         axs[0, i].set_title(f"Step {step}")
         axs[0, i].axis('off')
